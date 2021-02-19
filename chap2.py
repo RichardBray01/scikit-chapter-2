2e16ea3b959ed464a9756a8bc59954215b076042
Richard Bray <rjbray1@gmail.com>
2021-02-19 16:52:17 +0000

    tidied up printing

--- chap2.py
+++ chap2.py
@@ -94,9 +94,13 @@ from sklearn.ensemble import RandomForestRegressor
 
 from sklearn.model_selection import GridSearchCV
 
+# param_grid = [
+#     { 'n_estimators': [3,10,30], 'max_features': [2,4,6,8]},
+#     { 'bootstrap': [False], 'n_estimators': [3,10], 'max_features': [2,3,4]}
+# ]
 param_grid = [
-    { 'n_estimators': [3,10,30], 'max_features': [2,4,6,8]},
-    { 'bootstrap': [False], 'n_estimators': [3,10], 'max_features': [2,3,4]}
+    { 'n_estimators': [3], 'max_features': [2,4]},
+    { 'bootstrap': [False], 'n_estimators': [3], 'max_features': [2,3]}
 ]
 
 forest_reg = RandomForestRegressor()
@@ -106,9 +110,25 @@ grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
 
 grid_search.fit(housing_prepared, housing_labels)
 
-print(grid_search.best_params_)
-print(grid_search.best_estimator_)
+print("best params: ", grid_search.best_params_)
+print("best estimator: ", grid_search.best_estimator_)
 cvres = grid_search.cv_results_
 for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
     print(np.sqrt(-mean_score), params)
+
+feature_importances = grid_search.best_estimator_.feature_importances_
+
+extra_attribs = ["rooms_per_hhold", "pop_per_hhold", "bedrooms_per_room"]
+cat_encoder = full_pipeline.named_transformers_["cat"]
+cat_one_hot_attribs = list(cat_encoder.categories_[0])
+attributes = num_attribs + extra_attribs + cat_one_hot_attribs
+sorted_attribs = sorted(zip(feature_importances, attributes), reverse=True)
+print ("feature importances:")
+for i in sorted_attribs:
+    print(i)
+
+
+
+
+
 
